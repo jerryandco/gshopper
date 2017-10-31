8e59a43e9812969fdd0b85f73ebd24bdf842960e
danielcruser <danielcruser1@gmail.com>
2017-10-31 17:07:24 -0400

    gets orders data in json

--- server/api/index.js
+++ server/api/index.js
@@ -7,6 +7,8 @@ router.use('/candies', require('./candies'))
 
 router.use('/categories', require('./categories'))
 
+router.use('/orders', require('./orders'))
+
 router.use((req, res, next) => {
   const error = new Error('Not Found')
   error.status = 404
---
+++ server/api/orders.js
@@ -0,0 +1,9 @@
+const router = require('express').Router()
+const {Order} = require('../db/models')
+module.exports = router
+
+router.get('/', (req, res, next) => {
+  Order.findAll()
+    .then(orders => res.json(orders))
+    .catch(next)
+})
