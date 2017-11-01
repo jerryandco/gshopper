2f47dbd9ce575cea1a8a3fc22fd4b757bb4bc6dc
Hou In Choi <hichoi17@outlook.com>
2017-11-01 16:42:42 -0400

    feat(table) created join table and route related

--- server/db/models/orderCandy.js
+++ server/db/models/orderCandy.js
@@ -1,7 +1,5 @@
 const Sequelize = require('sequelize');
 const db = require('../db');
-const Order = require('./order');
-const Candy = require('./candy');
 
 const OrderCandy = db.define('ordercandy', {
     id: {
