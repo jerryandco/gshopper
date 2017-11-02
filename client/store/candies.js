0a76bff051b94f12ad957b75938d32216d915a77
Jerry <jerrymuzsik@icloud.com>
2017-11-02 12:36:07 -0400

    (feature) - added post, put, delete to the candies reducer/refactored code

--- client/store/candies.js
+++ client/store/candies.js
@@ -1,48 +1,83 @@
-import axios from 'axios'
-import history from '../history'
+import axios from 'axios';
+import history from '../history';
 
-/**
- * ACTION TYPES
- */
-const GET_CANDIES = 'GET_CANDIES'
+const GET_CANDIES = 'GET_CANDIES';
+const POST_CANDY = 'POST_CANDY';
+const DELETE_CANDY = 'DELETE_CANDY';
+const PUT_CANDY = 'PUT_CANDY';
 
-/**
- * INITIAL STATE
- */
+const getCandies = candies => ({ type: GET_CANDIES, candies });
+const postCandy = candy => ({ type: POST_CANDY, candy });
+const deleteCandy = candy => ({ type: DELETE_CANDY, candy });
+const putCandy = candy => ({ type: PUT_CANDY, candy });
 
- const initialState = {
-   allCandies: []
- }
-
-
-/**
- * ACTION CREATORS
- */
-const candiesAction = candies => ({type: GET_CANDIES, candies})
-
-export default function (state = initialState, action) {
-  const newState = Object.assign({}, state)
-    switch (action.type) {
-      case GET_CANDIES:
-      newState.allCandies = action.candies
-      return newState
-      default:
-        return newState
-    }
+export default function(candies = [], action) {
+  switch (action.type) {
+    case GET_CANDIES:
+      return action.candies;
+    case POST_CANDY:
+      return [action.candy, ...candies];
+    case PUT_CANDY:
+      var otherCandy = candies.filter(candy => candy.id !== action.candy.id);
+      return [action.candy, ...otherCandy];
+    case DELETE_CANDY:
+      return candies.filter(candy => candy.id !== action.candy.id);
+    default:
+      return candies;
   }
+}
 
-  //THUNK CREATOR
-  
-  export function fetchCandies(){
-    return function thunk(dispatch){
-      return axios
-      .get("/api/candies")
+export function fetchCandies() {
+  return function thunk(dispatch) {
+    return axios
+      .get('/api/candies')
       .then(res => res.data)
       .then(candies => {
-        const action = candiesAction(candies);
+        const action = getCandies(candies);
         dispatch(action);
       })
-      .catch(err => console.error(err))
-    }
-  }
+      .catch(err => console.error(err));
+  };
+}
 
+export const postCandyThunk = (newCandy, history) => {
+  return dispatch => {
+    return axios
+      .post('/api/candies', newCandy)
+      .then(res => {
+        return res.data;
+      })
+      .then(candy => {
+        const action = postCandy(candy);
+        dispatch(action);
+        //here we should return to a specific url related to admin
+      })
+      .catch(console.error);
+  };
+};
+
+export const putCandyThunk = candy => {
+  return dispatch => {
+    return axios
+      .put(`/api/candies/${candy.id}`, candy)
+      .then(res => {
+        return res.data;
+      })
+      .then(changedCandy => {
+        const action = putCandy(changedCandy);
+        dispatch(action);
+        //history or not?
+      })
+      .catch(console.error);
+  };
+};
+
+export const deleteCampusThunk = id => {
+  return dispatch => {
+    return axios.delete(`/api/candies/${id}`).then(candy => {
+      candy.id = +id;
+      const action = deleteCandy(candy);
+      dispatch(action);
+    });
+  };
+};
