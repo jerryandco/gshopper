ce1b412113ccbe2318008b05f7ae270c149081b8
Mitchell Stewart <mitchellwstewart@gmail.com>
2017-11-02 14:26:35 -0400

    feat(home component)

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -7,16 +7,69 @@ import { fetchCandies } from '../store/candies.js';
 import { fetchCategories } from '../store/categories.js';
 
 class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.generateFeaturedList = this.generateFeaturedList.bind(this);
+  }
+
   componentDidMount() {
     this.props.allCandiesFetch();
     this.props.allCategoriesFetch();
   }
 
+  generateFeaturedList = type => {
+    const featuredArr = [];
+    if (type === 'category') {
+      if (this.props.allCategories.length) {
+        for (let i = featuredArr.length; i <= 3; i++) {
+          let category = this.props.allCategories[
+            Math.floor(Math.random() * this.props.allCategories.length)
+          ];
+          if (featuredArr.indexOf(category) < 0) {
+            featuredArr.push(category);
+          }
+          else{
+              i--;
+          }
+        }
+      }
+    }
+
+    if (type === 'candy') {
+      if (this.props.allCandies.length) {
+        for (let i = featuredArr.length; i <= 5; i++) {
+          let candy = this.props.allCandies[
+            Math.floor(Math.random() * this.props.allCandies.length)
+          ];
+          if (featuredArr.indexOf(candy) < 0) {
+            featuredArr.push(candy);
+          }else{
+              i--
+          }
+        }
+      }
+    }
+    return featuredArr;
+  };
+
   render() {
     return (
       <div>
         <h1>Featured Categories</h1>
+        {this.generateFeaturedList('category').map(featCategory => (
+          <div key={featCategory.id}>
+            <p>{featCategory.name}</p>
+            <img src={featCategory.image} className="featured-image"/>
+          </div>
+        ))}
+
         <h1>Featured Candy</h1>
+        {this.generateFeaturedList('candy').map(featCandy => (
+          <div key={featCandy.id}>
+            <p>{featCandy.name}</p>
+            <img src={featCandy.image} className="featured-image" />
+          </div>
+        ))}
       </div>
     );
   }
