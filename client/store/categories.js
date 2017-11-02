42230c884f8c20762c822e37d6527f02f6f5bbae
Jerry <jerrymuzsik@icloud.com>
2017-11-02 18:32:43 -0400

    added post to categories

--- client/store/categories.js
+++ client/store/categories.js
@@ -1,49 +1,47 @@
-import axios from 'axios'
-import history from '../history'
+import axios from 'axios';
+import history from '../history';
+//will we reroute?
 
-/**
- * ACTION TYPES
- */
-const GET_CATEGORIES = 'GET_CATEGORIES'
-
-/**
- * INITIAL STATE
- */
-
-const initialState = {
-  allCategories: []
-}
+const GET_CATEGORIES = 'GET_CATEGORIES';
+const POST_CATEGORY = 'POST_CATEGORY';
 
 /**
  * ACTION CREATORS
  */
-const getCategories = categories => ({type: GET_CATEGORIES, categories})
+const getCategories = categories => ({ type: GET_CATEGORIES, categories });
+const postCategory = category => ({ type: POST_CATEGORY, category });
 
-
-
-export default function (state = initialState, action) {
-    switch (action.type) {
-      case GET_CATEGORIES:
-        return {
-          ...state,
-          allCategories: action.categories
-        }
-      default:
-        return state
-    }
+export default function(categories = [], action) {
+  switch (action.type) {
+    case GET_CATEGORIES:
+      return action.categories;
+    case POST_CATEGORY:
+      return [action.category, ...categories];
+    default:
+      return categories;
   }
+}
 
-  //THUNK CREATOR
+//THUNK CREATOR
 
-  export function fetchCategories(){
-    return function thunk(dispatch){
-      return axios
-      .get("/api/categories")
+export function fetchCategories() {
+  return function thunk(dispatch) {
+    return axios
+      .get('/api/categories')
       .then(res => res.data)
       .then(categories => {
         const action = getCategories(categories);
         dispatch(action);
       })
-      .catch(err => console.error(err))
-    }
-  }
+      .catch(err => console.error(err));
+  };
+}
+
+export const postCategoryThunk = category => dispatch => {
+  axios
+    .post('/api/categories', category)
+    .then(res => dispatch(postCategory(res.data)))
+    .catch(err =>
+      console.error(`Creating category: ${category} unsuccessful`, err)
+    );
+};
