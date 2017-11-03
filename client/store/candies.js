95384aa9787c32b65902dde3c88448d60afe6ee8
Jerry <jerrymuzsik@icloud.com>
2017-11-03 16:15:21 -0400

    fixed the merge conflicts

--- client/store/candies.js
+++ client/store/candies.js
@@ -1,49 +1,31 @@
-import axios from 'axios'
-import history from '../history'
+import axios from 'axios';
+import history from '../history';
 
-/**
- * ACTION TYPES
- */
 const GET_CANDIES = 'GET_CANDIES'
 const POST_CANDY = 'POST_CANDY';
 const DELETE_CANDY = 'DELETE_CANDY';
 const PUT_CANDY = 'PUT_CANDY';
-/**
- * INITIAL STATE
- */
 
-const initialState = {
-  allCandies: []
-}
-
-
-/**
- * ACTION CREATORS
- */
 const candiesAction = candies => ({ type: GET_CANDIES, candies })
-//const getCandies = candies => ({ type: GET_CANDIES, candies });
 const postCandyAction = candy => ({ type: POST_CANDY, candy });
 const deleteCandyAction = id => ({ type: DELETE_CANDY, id });
 const putCandyAction = candy => ({ type: PUT_CANDY, candy });
 
-export default function (state = initialState, action) {
-  const newState = Object.assign({}, state)
+export default function(candies = [], action) {
   switch (action.type) {
     case GET_CANDIES:
-      newState.allCandies = action.candies
-      return newState
+      return action.candies;
     case POST_CANDY:
-      newState.allCandies = [...newState.allCandies, action.candy];
-      return newState
+      return [action.candy, ...candies];
+    case PUT_CANDY:
+      var otherCandy = candies.filter(candy => candy.id !== action.candy.id);
+      return [action.candy, ...otherCandy];
     case DELETE_CANDY:
-      newState.allCandies = newState.allCandies.filter(candy => +candy.id !== +action.id);
-      return newState
+      return candies.filter(candy => candy.id !== action.candy.id);
     default:
-      return newState
+      return candies;
   }
 }
-
-//THUNK CREATOR
 
 export function fetchCandies() {
   return function thunk(dispatch) {
@@ -51,9 +33,24 @@ export function fetchCandies() {
       .get('/api/candies')
       .then(res => res.data)
       .then(candies => {
-        const action = candiesAction(candies);
+        const action = getCandies(candies);
         dispatch(action);
       })
+      .catch(err => console.error(err));
+  };
+}
+
+export const putCandyThunk = candy => {
+  return dispatch => {
+    return axios
+      .put(`/api/candies/${candy.id}`, candy)
+      .then(res => {
+        return res.data;
+      })
+      .then(changedCandy => {
+        const action = putCandy(changedCandy);
+        dispatch(action);
+        //history or not?
       .catch(err => console.error(err))
   }
 }
