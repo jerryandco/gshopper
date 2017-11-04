19b5577cd097a3ce3a6c558a2e8faa9dd7dbbf7b
Hou In Choi <hichoi17@outlook.com>
2017-11-04 12:41:17 -0400

    feat(category) able to add category to the db

--- client/components/AddCategory.js
+++ client/components/AddCategory.js
@@ -1,22 +1,39 @@
-import React, { Component } from "react";
-import { NavLink, withRouter } from "react-router-dom";
-import { connect } from "react-redux";
-import { postCategoryThunk } from "../store/categories.js";
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { postCategoryThunk } from '../store';
 
 class AddCategory extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      name: '',
+      description: ''
+    }
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.check = this.check.bind(this);
   }
+
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
   handleSubmit(event) {
     event.preventDefault();
-    let name = event.target.name.value;
-    let description = event.target.description.value;
-    const categoryObj = {
-      name,
-      description
-    };
-    this.props.postCategory(categoryObj, this.props.ownProps.history);
+    // let name = event.target.name.value;
+    // let description = event.target.description.value;
+    const categoryObj = Object.assign({},this.state);
+    if (event.target.image.value.length > 0) {
+      categoryObj[event.target.image.name] = event.target.image.value;
+    }
+    this.props.postCategory(categoryObj);
+  }
+
+  check(){
+    return this.state.name.length > 0 && this.state.description.length > 0;
   }
 
   render() {
@@ -25,24 +42,27 @@ class AddCategory extends Component {
         <form onSubmit={this.handleSubmit}>
           <label>
             Name:
-            <input type="text" name="name" autoFocus />
+            <input type="text" name="name" onChange={this.handleChange} autoFocus required />
           </label>
           <label>
             Description:
-            <input type="text" name="name" />
+            <input type="text" name="description" onChange={this.handleChange} required />
           </label>
-          <input type="submit" value="Submit" />
+          <label>
+            ImageUrl :
+          <input type="url" name="image" />
+          </label>
+          <input type="submit" value="Submit" disabled={!this.check()} />
         </form>
       </div>
     );
   }
 }
 
-const mapDispatchToProps = (dispatch, ownProps) => ({
-  postCategory: (category, history) => {
-    return dispatch(postCategoryThunk(category, history));
-  },
-  ownProps
+const mapDispatchToProps = (dispatch) => ({
+  postCategory: (category) => {
+    return dispatch(postCategoryThunk(category));
+  }
 });
 
 export default withRouter(connect(null, mapDispatchToProps)(AddCategory));
